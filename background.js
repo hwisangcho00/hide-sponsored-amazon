c276f61f9b8770970df26e0426a5dde34457a340
Joseph Cho <hwisang@seas.upenn.edu>
2023-08-18 18:34:44 -0400

    remove logging

--- background.js
+++ background.js
@@ -3,11 +3,9 @@ chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url?.startsWith("chrome://")) return undefined;
 
   if (tab.url?.startsWith("https://www.amazon.com")) {
-    console.log('Page uses History API and we heard a pushSate/replaceState.');
     await chrome.scripting.executeScript({
       target: {tabId: tab.id},
       files: ["scripts/content.js"]
     })
-    console.log("script execution should be complete")
   }
 })
